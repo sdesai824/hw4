0960b393489b0b37ac010623552612c3cd08c6d6
sdesai824 <sahil.desai@kellogg.northwestern.edu>
2023-03-04 13:30:18 +0000

    initial controller updates

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,10 +2,27 @@ class SessionsController < ApplicationController
   def new
   end
 
-  def create
+  def index
+    @user = User.find_by({ "email" => params["email"] })
+    if @user
+      if BCrypt::Password.new(@user["password"]) == params["password"]
+        session["user_id"] = @user["id"]
+        flash["notice"] = "Welcome."
+        redirect_to "/posts"
+      else
+        flash["notice"] = "Nope."
+        redirect_to "/sessions"
+      end
+    else
+      flash["notice"] = "Nope."
+      redirect_to "/sessions"
+    end
   end
 
   def destroy
+    session["user_id"] = nil
+    flash["notice"] = "Goodbye."
+    redirect_to "/sessions"
   end
 end
   
